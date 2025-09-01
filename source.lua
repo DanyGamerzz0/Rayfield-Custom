54cc68380e5b1f127b73a1493381fbbe06786cf7
DanyGamerzz0 <jifijifiliski@gmail.com>
2025-09-01 19:23:03 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -7,7 +7,7 @@
 	iRay   | Programming
 	Max    | Programming
 	Damian | Programming
-	27
+	29
 ]]
 
 if debugX then
@@ -3692,9 +3692,9 @@ end
 	Slider.Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Slider.Main.UIStroke.Color = Color3.fromRGB(255, 255, 255)
 	Slider.Main.Shadow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Slider.Main.Progress.BackgroundColor3 = Color3.fromRGB(220, 220, 220)
-	Slider.Main.Progress.UIStroke.Color = Color3.fromRGB(255, 255, 255)
-	Slider.Title.Size = UDim2.new(0, 100,0, 14)
+	Slider.Main.Progress.BackgroundColor3 = Color3.fromRGB(170, 170, 170)
+	Slider.Main.Progress.UIStroke.Color = Color3.fromRGB(170, 170, 170)
+	Slider.Title.Size = UDim2.new(0, 120,0, 14)
 
 	TweenService:Create(Slider, TweenInfo.new(0.7, Enum.EasingStyle.Exponential), {BackgroundTransparency = 0}):Play()
 	TweenService:Create(Slider.UIStroke, TweenInfo.new(0.7, Enum.EasingStyle.Exponential), {Transparency = 0}):Play()
