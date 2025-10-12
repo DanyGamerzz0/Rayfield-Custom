7ffcfedd10f973460af9d44acd43cb4001827c6d
DanyGamerzz0 <jifijifiliski@gmail.com>
2025-10-13 00:21:39 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -3915,7 +3915,7 @@ end
 
 	return SliderSettings
 end
---53.0f
+--54.0f
 function Tab:CreateCollapsible(CollapsibleSettings)
     local CollapsibleValue = {}
     local IsExpanded = CollapsibleSettings.DefaultExpanded or false
@@ -3944,7 +3944,7 @@ function Tab:CreateCollapsible(CollapsibleSettings)
     Arrow.Position = UDim2.new(0, 12, 0, 14.5)  -- Fixed Y position (centered in 45px)
     Arrow.AnchorPoint = Vector2.new(0, 0)
     Arrow.BackgroundTransparency = 1
-    Arrow.Image = "rbxassetid://2418686959"
+    Arrow.Image = getIcon("chevron-right")
     Arrow.ImageColor3 = SelectedTheme.TextColor
     Arrow.Rotation = IsExpanded and 90 or 0
     Arrow.Parent = Collapsible
