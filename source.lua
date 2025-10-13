5e5992e0e4d5b75fdc766303044a6abe87f4b4d8
DanyGamerzz0 <jifijifiliski@gmail.com>
2025-10-13 14:45:55 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -3915,7 +3915,7 @@ end
 
 	return SliderSettings
 end
---63.0f
+--61.0f
 function Tab:CreateCollapsible(CollapsibleSettings)
     local CollapsibleValue = {}
     local IsExpanded = CollapsibleSettings.DefaultExpanded or false
@@ -4116,8 +4116,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
@@ -4128,8 +4132,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
@@ -4140,8 +4148,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
@@ -4152,8 +4164,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
@@ -4164,8 +4180,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
@@ -4176,8 +4196,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
@@ -4272,8 +4296,12 @@ function Tab:CreateCollapsible(CollapsibleSettings)
         element.Parent = Container
         childCount = childCount + 1
         element.LayoutOrder = childCount
-        element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
-        element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+if element and element.Parent then
+        pcall(function()
+            element:GetPropertyChangedSignal("Size"):Connect(UpdateContainerSize)
+            element:GetPropertyChangedSignal("Visible"):Connect(UpdateContainerSize)
+        end)
+    end
         UpdateContainerSize()
         return element
     end
